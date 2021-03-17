efa8154b9e6f435489cb627b4ef52cd28884a36f
vova99 <mr.oleksiyenko@gmail.com>
2021-03-17 19:31:59 +0200

    init

--- src/main/java/com/charlie/zoo/entity/dto/PackageTypeDto.java
+++ src/main/java/com/charlie/zoo/entity/dto/PackageTypeDto.java
@@ -1,11 +1,16 @@
 package com.charlie.zoo.entity.dto;
 
-import com.charlie.zoo.entity.Product;
+import com.charlie.zoo.entity.PackageType;
 import com.charlie.zoo.enums.StatusOfEntity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
 import java.math.BigDecimal;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class PackageTypeDto {
     private int id;
 
@@ -14,12 +19,25 @@ public class PackageTypeDto {
     private BigDecimal packSize;
     private String packType;
     private boolean onSale;
-
-
     private int countOfProduct;
+    private boolean statusOfEntity;
 
+    public static PackageType convertToPackageType(PackageTypeDto packageTypeDto){
+        PackageType packageType = new PackageType();
 
+        packageType.setId(packageTypeDto.id);
+        packageType.setPrice(packageTypeDto.price);
+        packageType.setNewPrice(packageTypeDto.newPrice);
+        packageType.setPackSize(packageTypeDto.packSize);
+        packageType.setPackType(packageTypeDto.packType);
+        packageType.setOnSale(packageTypeDto.onSale);
+        packageType.setCountOfProduct(packageTypeDto.countOfProduct);
+        if(packageTypeDto.statusOfEntity){
+            packageType.setStatusOfEntity(StatusOfEntity.ACTIVE);
+        }else{
+            packageType.setStatusOfEntity(StatusOfEntity.ARCHIVE);
+        }
 
-    @Enumerated(EnumType.STRING)
-    private StatusOfEntity statusOfEntity = StatusOfEntity.ACTIVE;
+        return packageType;
+    }
 }
