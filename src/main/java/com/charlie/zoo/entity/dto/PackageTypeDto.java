25cda780c74306e7b63b0511c68dce41e8013968
vova99 <mr.oleksiyenko@gmail.com>
2021-03-26 20:47:04 +0200

    init

--- src/main/java/com/charlie/zoo/entity/dto/PackageTypeDto.java
+++ src/main/java/com/charlie/zoo/entity/dto/PackageTypeDto.java
@@ -4,8 +4,6 @@ import com.charlie.zoo.entity.PackageType;
 import com.charlie.zoo.enums.StatusOfEntity;
 import com.google.gson.Gson;
 import lombok.Data;
-import net.minidev.json.JSONArray;
-import net.minidev.json.JSONObject;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
@@ -19,6 +17,14 @@ public class PackageTypeDto {
     private double newPrice;
     private double packSize;
     private double discount;
+
+    private double wholeSalePrice;
+    private double wholeSaleNewPrice;
+    private double wholeSaleDiscount;
+    private boolean wholeSaleOnSale;
+    private boolean wholeSaleStatus;
+
+
     private String packType;
     private boolean onSale;
     private int countOfProduct;
@@ -27,27 +33,27 @@ public class PackageTypeDto {
     public PackageTypeDto() {
     }
 
-    public static PackageType convertToPackageType(String[] packArray){
-        if(packArray.length>=8) {
-            PackageType packageType = new PackageType();
-            packageType.setPackType(packArray[0]);
-            packageType.setPackSize(BigDecimal.valueOf(Double.parseDouble(packArray[1])));
-            packageType.setCountOfProduct(Integer.parseInt(packArray[2]));
-            if (packArray[3].toLowerCase().equals("true")) {
-                packageType.setStatusOfEntity(StatusOfEntity.ACTIVE);
-            } else {
-                packageType.setStatusOfEntity(StatusOfEntity.ARCHIVE);
-            }
-            packageType.setPrice(BigDecimal.valueOf(Double.parseDouble(packArray[4])));
-            packageType.setOnSale(packArray[5].toLowerCase().equals("true"));
-            packageType.setNewPrice(BigDecimal.valueOf(Double.parseDouble(packArray[6])));
-            if (!packArray[7].isEmpty()) {
-                packageType.setId(Integer.parseInt(packArray[7]));
-            }
-            return packageType;
-        }
-        return null;
-    }
+//    public static PackageType convertToPackageType(String[] packArray){
+//        if(packArray.length>=8) {
+//            PackageType packageType = new PackageType();
+//            packageType.setPackType(packArray[0]);
+//            packageType.setPackSize(BigDecimal.valueOf(Double.parseDouble(packArray[1])));
+//            packageType.setCountOfProduct(Integer.parseInt(packArray[2]));
+//            if (packArray[3].toLowerCase().equals("true")) {
+//                packageType.setStatusOfEntity(StatusOfEntity.ACTIVE);
+//            } else {
+//                packageType.setStatusOfEntity(StatusOfEntity.ARCHIVE);
+//            }
+//            packageType.setPrice(BigDecimal.valueOf(Double.parseDouble(packArray[4])));
+//            packageType.setOnSale(packArray[5].toLowerCase().equals("true"));
+//            packageType.setNewPrice(BigDecimal.valueOf(Double.parseDouble(packArray[6])));
+//            if (!packArray[7].isEmpty()) {
+//                packageType.setId(Integer.parseInt(packArray[7]));
+//            }
+//            return packageType;
+//        }
+//        return null;
+//    }
 
     public static List<PackageType> getArrayOfPackageTypes(String types){
         List<PackageType> packageTypes = new ArrayList<>();
@@ -72,6 +78,12 @@ public class PackageTypeDto {
         packageTypeDto.onSale=packageType.isOnSale();
         packageTypeDto.countOfProduct=packageType.getCountOfProduct();
 
+        packageTypeDto.wholeSaleDiscount=packageType.getWholeSaleDiscount().doubleValue();
+        packageTypeDto.wholeSalePrice=packageType.getWholeSalePrice().doubleValue();
+        packageTypeDto.wholeSaleNewPrice=packageType.getWholeSalePrice().doubleValue();
+        packageTypeDto.wholeSaleOnSale=packageType.isWholeSaleOnSale();
+        packageTypeDto.wholeSaleStatus=packageType.isWholeSaleStatus();
+
         if(packageType.getStatusOfEntity()==StatusOfEntity.ACTIVE) {
             packageTypeDto.statusOfEntity=true;
         }else {
@@ -92,6 +104,15 @@ public class PackageTypeDto {
         packageType.setOnSale(packageTypeDto.onSale);
         packageType.setCountOfProduct(packageTypeDto.countOfProduct);
         packageType.setDiscount(BigDecimal.valueOf(packageTypeDto.discount));
+
+
+        packageType.setWholeSalePrice(BigDecimal.valueOf(packageTypeDto.wholeSalePrice));
+        packageType.setWholeSaleNewPrice(BigDecimal.valueOf(packageTypeDto.wholeSaleNewPrice));
+        packageType.setWholeSaleDiscount(BigDecimal.valueOf(packageTypeDto.wholeSaleDiscount));
+
+        packageType.setWholeSaleOnSale(packageTypeDto.wholeSaleOnSale);
+        packageType.setWholeSaleStatus(packageTypeDto.wholeSaleStatus);
+
         if(packageTypeDto.statusOfEntity){
             packageType.setStatusOfEntity(StatusOfEntity.ACTIVE);
         }else{
