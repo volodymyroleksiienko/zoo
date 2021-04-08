2bd281ed186efb2bddc9ee140a05923084c77e90
vova99 <mr.oleksiyenko@gmail.com>
2021-04-09 00:48:26 +0300

    init

--- src/main/java/com/charlie/zoo/controller/admin/AdminOrderController.java
+++ src/main/java/com/charlie/zoo/controller/admin/AdminOrderController.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -18,4 +19,7 @@ public class AdminOrderController {
         model.addAttribute("orders",orderService.findAll());
         return "admin/orders";
     }
+
+//    @PostMapping("/modalUpdate")
+//    public S
 }
