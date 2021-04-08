6fa6e1fbe58b3782d96a6d2878b7f79b0cab38ee
vova99 <mr.oleksiyenko@gmail.com>
2021-04-08 17:09:16 +0300

    init

--- src/main/java/com/charlie/zoo/controller/admin/AdminOrderController.java
+++ src/main/java/com/charlie/zoo/controller/admin/AdminOrderController.java
@@ -1,4 +1,21 @@
 package com.charlie.zoo.controller.admin;
 
+import com.charlie.zoo.service.OrderService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin/orders")
+@AllArgsConstructor
 public class AdminOrderController {
+    private final OrderService orderService;
+
+    @GetMapping
+    public String get(Model model){
+        model.addAttribute("orders",orderService.findAll());
+        return "admin/orders";
+    }
 }
