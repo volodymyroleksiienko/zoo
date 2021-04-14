ba0bcb048917f6f6551ee65dc52da7b271c0ed5e
roman <romantymochko@gmail.com>
2021-04-14 13:27:05 +0200

    banners

--- src/main/java/com/charlie/zoo/controller/admin/AdminBannersController.java
+++ src/main/java/com/charlie/zoo/controller/admin/AdminBannersController.java
@@ -1,10 +1,13 @@
 package com.charlie.zoo.controller.admin;
 
 
+import com.charlie.zoo.entity.Producer;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 @RequestMapping("/admin/banners")
@@ -16,4 +19,9 @@ public class AdminBannersController {
         return "admin/banners";
     }
 
+    @PostMapping({"/edit"})
+    public String editBanner(MultipartFile multipartFile){
+//        bannerService.save(multipartFile);
+        return "redirect:/admin/banners";
+    }
 }
