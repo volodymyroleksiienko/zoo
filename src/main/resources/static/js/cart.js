55fbffff7f66379dbe6b29b7de796920c5027d91
roman <romantymochko@gmail.com>
2021-05-06 18:02:40 +0200

    cart

--- src/main/resources/static/js/cart.js
+++ src/main/resources/static/js/cart.js
@@ -18,41 +18,47 @@ function addToCartItem(btnObj) {
     let packageId = jsonObj["id"];
     let sideBar = $('#sidebarCart');
 
-    $.ajax({
-        url: '/addToCart/'+packageId,
-        type: 'post',
-        success: function(dto){
-            sideBar.empty();
-            let orderDetails = dto['orderDetails'];
-            for (let i=0; i<orderDetails.length; i++){
-                let itemsSum = 0.0;
-                if (orderDetails[i].packageTypeDto.onSale===true){
-                    itemsSum = parseInt(orderDetails[i].count)*parseFloat(orderDetails[i].packageTypeDto.newPrice);
-                }else {
-                    itemsSum = parseInt(orderDetails[i].count)*parseFloat(orderDetails[i].packageTypeDto.price);
+    if (btnObj.classList.contains('added')){
+        btnObj.classList.toggle('added');
+    }else {
+        $.ajax({
+            url: '/addToCart/' + packageId,
+            type: 'post',
+            success: function (dto) {
+                btnObj.classList.toggle('added');
+                sideBar.empty();
+                let orderDetails = dto['orderDetails'];
+                for (let i = 0; i < orderDetails.length; i++) {
+                    let itemsSum = 0.0;
+                    if (orderDetails[i].packageTypeDto.onSale === true) {
+                        itemsSum = parseInt(orderDetails[i].count) * parseFloat(orderDetails[i].packageTypeDto.newPrice);
+                    } else {
+                        itemsSum = parseInt(orderDetails[i].count) * parseFloat(orderDetails[i].packageTypeDto.price);
+                    }
+                    sideBar.prepend('<div class="shp__single__product">\n' +
+                        '                <div class="shp__pro__thumb">\n' +
+                        '                    <a href="/singleProduct/' + orderDetails[i].packageTypeDto.productDto.id + '">\n' +
+                        '                        <img src="/shop/getImgByProductId/' + orderDetails[i].packageTypeDto.productDto.id + '" alt="product images">\n' +
+                        '                    </a>\n' +
+                        '                </div>\n' +
+                        '                <div class="shp__pro__details">\n' +
+                        '                    <h2><a href="/singleProduct/' + orderDetails[i].packageTypeDto.productDto.id + '">' + orderDetails[i].packageTypeDto.productDto.name + ' (' + orderDetails[i].packageTypeDto.packSize + ' ' + orderDetails[i].packageTypeDto.packType + ')</a></h2>\n' +
+                        '                    <span class="quantity">Кількість: ' + orderDetails[i].count + '</span>\n' +
+                        '                    <span class="shp__price">' + itemsSum.toFixed(2) + ' UAH' + '</span>\n' +
+                        '                </div>\n' +
+                        '                <div class="remove__btn">\n' +
+                        '                    <button onclick="removeItemFromCart(' + orderDetails[i].id + ',\'' + orderDetails[i].orderInfoId + '\')" title="Видалити з кошика"><i class="ti-close"></i></button>\n' +
+                        '                </div>\n' +
+                        '            </div>');
                 }
-                sideBar.prepend('<div class="shp__single__product">\n' +
-                    '                <div class="shp__pro__thumb">\n' +
-                    '                    <a href="/singleProduct/'+orderDetails[i].packageTypeDto.productDto.id+'">\n' +
-                    '                        <img src="/shop/getImgByProductId/'+orderDetails[i].packageTypeDto.productDto.id+'" alt="product images">\n' +
-                    '                    </a>\n' +
-                    '                </div>\n' +
-                    '                <div class="shp__pro__details">\n' +
-                    '                    <h2><a href="/singleProduct/'+orderDetails[i].packageTypeDto.productDto.id+'">'+orderDetails[i].packageTypeDto.productDto.name+' ('+orderDetails[i].packageTypeDto.packSize+' '+orderDetails[i].packageTypeDto.packType+')</a></h2>\n' +
-                    '                    <span class="quantity">Кількість: '+orderDetails[i].count+'</span>\n' +
-                    '                    <span class="shp__price">'+itemsSum.toFixed(2)+' UAH'+'</span>\n' +
-                    '                </div>\n' +
-                    '                <div class="remove__btn">\n' +
-                    '                    <button onclick="removeItemFromCart('+orderDetails[i].id+',\''+orderDetails[i].orderInfoId+'\')" title="Видалити з кошика"><i class="ti-close"></i></button>\n' +
-                    '                </div>\n' +
-                    '            </div>');
+                calculateCartTotal();
+            },
+            error: function () {
+                alert("Помилка! Спробуйте ще раз");
             }
-            calculateCartTotal();
-        },
-        error: function () {
-            alert("Помилка! Спробуйте ще раз");
-        }
-    });
+        });
+    }
+
 }
 
 function removeItemFromCart(dtid, uuid) {
