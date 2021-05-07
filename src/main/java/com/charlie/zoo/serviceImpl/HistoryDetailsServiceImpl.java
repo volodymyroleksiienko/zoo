f46d5a62af50b0d5948b56a4278eab52f4789983
vova99 <mr.oleksiyenko@gmail.com>
2021-05-07 19:45:06 +0300

    init

--- src/main/java/com/charlie/zoo/serviceImpl/HistoryDetailsServiceImpl.java
+++ src/main/java/com/charlie/zoo/serviceImpl/HistoryDetailsServiceImpl.java
@@ -1,9 +1,11 @@
 package com.charlie.zoo.serviceImpl;
 
 import com.charlie.zoo.entity.HistoryDetails;
+import com.charlie.zoo.entity.PackageType;
 import com.charlie.zoo.entity.ProductHistory;
 import com.charlie.zoo.jpa.HistoryDetailsJpa;
 import com.charlie.zoo.service.HistoryDetailsService;
+import com.charlie.zoo.service.PackageTypeService;
 import com.charlie.zoo.service.ProductHistoryService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -15,12 +17,23 @@ import java.util.List;
 @AllArgsConstructor
 public class HistoryDetailsServiceImpl implements HistoryDetailsService {
     private final HistoryDetailsJpa historyDetailsJpa;
+    private final PackageTypeService packageTypeService;
     private final ProductHistoryService productHistoryService;
 
 
     @Override
     public HistoryDetails save(HistoryDetails details) {
+        int count = 0;
+        if (details.getId()==0){
+            count = details.getCount();
+        }
+
         HistoryDetails detailsDB = historyDetailsJpa.save(details);
+        if(count>0) {
+            PackageType type = detailsDB.getPackageType();
+            type.setCountOfProduct(type.getCountOfProduct() + count);
+            packageTypeService.save(type);
+        }
         if(detailsDB.getProductHistory()!=null) {
             productHistoryService.countSummaryPrice(detailsDB.getProductHistory().getId());
         }
@@ -31,6 +44,14 @@ public class HistoryDetailsServiceImpl implements HistoryDetailsService {
     public HistoryDetails update(int historyId,HistoryDetails details) {
         HistoryDetails detailsDB = findById(details.getId());
         if(detailsDB!=null){
+            System.out.println(details);
+            System.out.println(detailsDB);
+            int count = details.getCount()-detailsDB.getCount();
+            if(count!=0) {
+                PackageType type = detailsDB.getPackageType();
+                type.setCountOfProduct(type.getCountOfProduct() + count);
+                packageTypeService.save(type);
+            }
             detailsDB.setCount(details.getCount());
             detailsDB.setPrice(details.getPrice());
             detailsDB.setSum(details.getCount()*details.getPrice());
