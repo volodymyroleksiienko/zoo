46adcd6edfee6fca37d3af6966cef462ee943c0d
vova99 <mr.oleksiyenko@gmail.com>
2021-06-04 12:28:29 +0300

    order without cart

--- src/main/java/com/charlie/zoo/ZooApplication.java
+++ src/main/java/com/charlie/zoo/ZooApplication.java
@@ -1,14 +1,26 @@
 package com.charlie.zoo;
 
+import com.charlie.zoo.controller.bot.Bot;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.telegram.telegrambots.ApiContextInitializer;
 
 @SpringBootApplication
 @EnableTransactionManagement
-public class ZooApplication {
+public class ZooApplication implements CommandLineRunner {
+
+
+	@Override
+	public void run(String... args) throws Exception {
+		Bot bot = new Bot("CharlieZooServiceBot", "1781017340:AAHbCNfdxCM63L5kyQyBU0BCUtEvsjXWBwE");
+		bot.botConnect();
+
+	}
 
 	public static void main(String[] args) {
+		ApiContextInitializer.init();
 		SpringApplication.run(ZooApplication.class, args);
 	}
 
